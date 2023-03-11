2e348f79b1f626c23efc8f3deb3a3abfdc8414b6
Mohamed Essam <mohamedessam9819@gmail.com>
2023-03-11 17:13:44 +0200

    Commit magdy& marina

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require("express");
 var ProductsRoutes = require("./Routes/products")
 var OffersRoutes = require("./Routes/offers")
 var ReviewsRoutes = require("./Routes/reviews")
+//Marina
+var catRoute = require("./routes/category")
+var paymentRoute = require("./routes/payment") 
+
+//////////magdy
+const copounsRoutes=require("./routes/copouns")
 
 const app = express();
 const port = process.env.PORT 
@@ -12,6 +18,13 @@ require("./db/mongoos");
 app.use("/Products",ProductsRoutes)
 app.use("/Offers",OffersRoutes)
 app.use("/Reviews",ReviewsRoutes)
+
+//Marina
+app.use('/category', catRoute)
+app.use('/payment', paymentRoute)
+
+//magdy 
+app.use("/copouns",copounsRoutes)
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(` app listening`));
