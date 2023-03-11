ede79c71116ca6f748f5a14e73daf729532a880d
Hussam Hassan <35435902+hossamHassanIbrahem@users.noreply.github.com>
2023-03-11 22:13:31 +0200

    translate last ver
    gfgf

--- routes/category.js
+++ routes/category.js
@@ -1,24 +1,29 @@
-
-const express = require('express');
+const express = require("express");
 var router = express.Router();
-var {createCategory,
-    getCategories,
-    getCategory,
-    updateCategory,
-    deleteCategory
-} = require('../controllers/category.js')
-const fs = require('fs')
-var catModel = require('../models/category')
-const {auth}= require('../middeleware/auth')
+var {
+  createCategory,
+  getCategories,
+  getCategory,
+  updateCategory,
+  deleteCategory,
+} = require("../controllers/category.js");
+const fs = require("fs");
+var catModel = require("../models/category");
+const { auth } = require("../middeleware/auth");
 
+router.route("/").get(getCategories).post(createCategory);
+router
+  .route("/:id")
+  .get(getCategory)
+  .put(updateCategory)
+  .delete(deleteCategory);
 
-router.route('/').get(getCategories).post(createCategory);
-router.route('/:id').get(getCategory).put(updateCategory).delete(deleteCategory)
+///
+/**
+ * app.get('/payment', (req, res) => {
+  // Redirect to /demo2
+  res.redirect(' send data to  api ');
+});
+ */
 
-
-
-
-
-
-
-module.exports=router;
+module.exports = router;
