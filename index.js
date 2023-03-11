7f7ebfb7b899055d21aa1daef82ad5454b8920f5
Ahmed-magdy227 <123758286+Ahmed-magdy227@users.noreply.github.com>
2023-03-11 16:27:09 +0200

    commit my copouns ahmed magdy

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ var ProductsRoutes = require("./Routes/products")
 var OffersRoutes = require("./Routes/offers")
 var ReviewsRoutes = require("./Routes/reviews")
 
+//////////magdy
+const copounsRoutes=require("./routes/copouns")
+
+
+// app.use(express.json())
+
+
 const app = express();
 const port = process.env.PORT 
 require("./db/mongoos");
@@ -12,6 +19,7 @@ require("./db/mongoos");
 app.use("/Products",ProductsRoutes)
 app.use("/Offers",OffersRoutes)
 app.use("/Reviews",ReviewsRoutes)
+app.use("/copouns",copounsRoutes)
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(` app listening`));
