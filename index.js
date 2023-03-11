884a5bd9159a443eb1cb7dadef8f73d635ef3608
Mohamed Essam <mohamedessam9819@gmail.com>
2023-03-11 15:50:39 +0200

    commit products reviews offers

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+//Essam
+var ProductsRoutes = require("./Routes/products")
+var OffersRoutes = require("./Routes/offers")
+var ReviewsRoutes = require("./Routes/reviews")
+
 const app = express();
 const port = process.env.PORT 
 require("./db/mongoos");
+
+//Essam
+app.use("/Products",ProductsRoutes)
+app.use("/Offers",OffersRoutes)
+app.use("/Reviews",ReviewsRoutes)
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(` app listening`));
---
+++ routes/products.js
@@ -0,0 +1,123 @@
+const express = require("express");
+var { createProduct, updateProductById, getAll, gettAllById, deleteProductById ,gettAllByCat,gettAllByOffer,gettAllByReview} = require("../Controllers/products");
+var router = express.Router()
+
+//get all
+router.get("/",async (req, res, next) => {
+    try{
+        var Products=  await getAll()
+        res.status(200).json(Products)
+    }catch(err){
+        res.json({message:err.message})
+    }
+
+})
+//get all limits
+// router.get("/",async (req, res, next) => {
+//     var limit = parseInt(req.query.limit) || 10
+//     var skip = parseInt(req.query.skip)     || 0
+//     try{
+//         var Products=  await getAll(limit,skip)
+//         res.status(200).json(Products)
+//     }catch(err){
+//         res.json({message:err.message})
+//     }
+
+// })
+
+
+//get by id
+router.get("/:id", async (req, res, next) => {
+    var { id } = req.params
+    try{
+        var Products=  await gettAllById(id)
+        res.status(200).json(Products)
+    }catch(err){
+        res.json({message:err.message})
+    }
+})
+//prod count
+// router.get("/count", async (req, res, next) => {
+//     var { id } = req.params
+//     try{
+//         var Products=  await gettAllById(id)
+//         res.status(200).json(Products)
+//     }catch(err){
+//         res.json({message:err.message})
+//     }
+// })
+
+//get by cat
+router.get("/Categories/:id", async (req, res, next) => {
+    var { id } = req.params
+    try{
+        var Products=  await gettAllByCat(id)
+        res.status(200).json(Products)
+    }catch(err){
+        res.json({message:err.message})
+    }
+})
+
+//get prod by offer
+router.get("/Offers/:id", async (req, res, next) => {
+    var { id } = req.params
+    try{
+        var Products=  await gettAllByOffer(id)
+        res.status(200).json(Products)
+    }catch(err){
+        res.json({message:err.message})
+    }
+})
+
+// get prod by review
+router.get("/Reviews/:id", async (req, res, next) => {
+    var { id } = req.params
+    try{
+        var Products=  await gettAllByReview(id)
+        res.status(200).json(Products)
+    }catch(err){
+        res.json({message:err.message})
+    }
+})
+
+//update
+router.patch("/:id", async (req, res, next) => {
+    var id = req.params.id
+    var product=[req.body] //add status //{title:[]=[]}
+  try {
+//    var updtaedProdutcs=  await updateProductById(id,product)
+var updtaedProdutcs=  await updateProductById(id,product)
+
+    res.json(updtaedProdutcs)
+    
+  } catch (err) {
+    res.json({message:err.message})
+
+  }
+
+})
+
+
+
+//delete
+router.delete("/:id", async (req, res, next) => {
+    var id = req.params.id
+    var deleteProduct = await deleteProductById(id)
+    res.json(deleteProduct)
+
+})
+
+//add create
+router.post("/", async(req, res, next) => {
+    var product = req.body
+    // todo.userId =req.userId
+    var savedProduct =await createProduct(product)
+
+    res.status(201).json(savedProduct) // created
+// } catch (err) {
+//     res.json({message:err.message})
+
+//   }
+})
+
+module.exports=router
