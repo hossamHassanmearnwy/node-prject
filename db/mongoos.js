97468cec0e9288cc3f6691b12c6e722d9ec45837
Hussam Hassan <35435902+hossamHassanIbrahem@users.noreply.github.com>
2023-03-27 01:12:43 +0200

    12
    12

--- db/mongoos.js
+++ db/mongoos.js
@@ -2,10 +2,9 @@ const mongoose = require("mongoose");
 //import { env } from 'process';
 // mongoose.connect(process.env.DB_URL);
 const db_uri = process.env.D_URL;
-
-
 const dbConnection = () => {
-  mongoose.connect(db_uri)
+  mongoose
+    .connect(db_uri)
     .then((conn) => {
       console.log(`Successfully Connection on: ${conn.connection.host}`);
     })
