2a79dfcf81494d98865819454fb737f13763b544
Fatma-Mahmoud-55 <113183957+Fatma-Mahmoud-55@users.noreply.github.com>
2023-03-26 20:38:34 +0200

    Update cart.js

--- routes/cart.js
+++ routes/cart.js
@@ -12,6 +12,9 @@ router.get("/:id", isAdmin, cartControl.getCartByUserID);
 // ADD TO CART
 router.post("/", isUser, cartControl.addToCart);
 
+// edit
+router.put("/update/:id", isUser, cartControl.updateQuantity);
+
 // DELETE CART
 router.delete("/:id", isUser, cartControl.deleteFromCart);
 
